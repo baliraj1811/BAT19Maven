91034e5d7af66bf969964b647849d3d326f194e2
baliraj1811 <baliraj1811@gmail.com>
2023-11-29 22:23:14 +0530

    BAT 19 21 Aug 2023 Bdd project files

---
+++ src/test/java/stepdefinations/Base.java
@@ -0,0 +1,18 @@
+package stepdefinations;
+
+import org.openqa.selenium.WebDriver;
+
+import pageObject.AddNewCustomerPage;
+import pageObject.AdminPage;
+import utilities.ReadConfig;
+
+public class Base {
+
+	public WebDriver driver;
+	
+	public AdminPage ad;
+	
+	public ReadConfig readConfig;
+	
+	public AddNewCustomerPage addCust;
+}
